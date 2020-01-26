462d49a90f63c44471dd9c5feee4629c40b0a0e3
Kyle Polich <kyle@dataskeptic.com>
2020-01-26 12:14:12 -0800

    blog

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const AWS = require('aws-sdk');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+var s3 = new AWS.S3({region: 'us-west-2'});
+
+const bucket_name = "serverless-crawl";
 
 var app = express();
 
@@ -24,6 +29,27 @@ app.get('/podcasts', (req, res) => res.render('pages/podcasts'))
 
 app.get('/insecure', function (req, res) {
   res.send('Dangerous!');
+});
+
+app.get('/blog/*', function(req, res) {
+    var key = req.path;
+    if(key.indexOf(".html") === -1) {
+        key = key + ".html";
+    }
+    const root = 'user/test/apps/publishingtools/outbox/data-skeptic/blog/master/'
+    key = root + key.substring(6, key.length)
+    console.log(key)
+    var getParams = {
+        Bucket: bucket_name,
+        Key: key
+    }
+    s3.getObject(getParams, function(err, data) {
+        if (err)
+            return err;
+        let body = data.Body.toString('utf-8');
+        res.render('pages/blog', {body})
+    });
+
 });
 
 app.get('*', (req, res) => res.send('Page Not found 404'));
