7e1340980a41e6e364dea8e5be1b8bfdaf9c94fe
Kyle Polich <kyle@dataskeptic.com>
2020-01-25 22:56:51 -0800

    fix title

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('pages/index'))
+app.get('/podcasts', (req, res) => res.render('pages/podcasts'))
 
 app.get('/insecure', function (req, res) {
   res.send('Dangerous!');
