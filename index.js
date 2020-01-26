2cf52d43e29367d5ba98cf609aadf97bf811c4d1
Kyle Polich <kyle@dataskeptic.com>
2020-01-25 22:24:26 -0800

    https

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+
+// Don't redirect if the hostname is `localhost:port` or the route is `/insecure`
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.get('/insecure', function (req, res) {
+  res.send('Dangerous!');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
