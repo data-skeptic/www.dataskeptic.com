51c9b933696429414b573ffde5e012d6f6744ab3
Kyle Polich <kyle@dataskeptic.com>
2020-09-12 22:45:35 -0700

    fixes

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.get('/privacy-policy', async (req, res) => {
 });
 
 app.get('/podcasts/dataskeptic', async (req, res) => {
-    const title = "Data Skeptic: Interpretability";
+    const title = "Data Skeptic";
     const episodes = await get_episodes('episodes/2020/');
     res.render('pages/podcasts', {title, episodes});
 });
